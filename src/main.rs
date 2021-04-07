0b0a504849ba0e2c4c918e37c03adbdb5827c3de
Kyle Goldbeck <kyle.goldbeck@gmail.com>
2021-04-06 20:14:06 -0700

    Finished and working

--- src/main.rs
+++ src/main.rs
@@ -8,103 +8,77 @@
 // Puzzle isn't being read correctly and causing errors
 // fix index out of bounds error if possible
 
+use std::env;
 use std::fs;
 
 const DIM: usize = 9;
 const SUBGRID_DIM: usize = 3;
-const ROWLEN: usize = 9;
+const VAL_DIM: usize = 10;
 
 
 struct Puzzle {
-    values:  [[usize; DIM]; DIM],
-    isFixed: [[ bool; DIM]; DIM],
-    colHasVal: [[ bool; DIM ]; DIM ],
-    rowHasVal: [[ bool; DIM]; DIM],
-    subgridHasVal: [[[bool; DIM ]; SUBGRID_DIM]; SUBGRID_DIM],
+    values:  [[usize; VAL_DIM]; DIM],
+    is_fixed: [[ bool; DIM]; DIM],
+    col_has_val: [[ bool; VAL_DIM ]; DIM ],
+    row_has_val: [[ bool; VAL_DIM]; DIM],
+    subgrid_has_val: [[[bool; VAL_DIM ]; SUBGRID_DIM]; SUBGRID_DIM],
 }
-
 
 impl Default for Puzzle {
     fn default () -> Puzzle{
         Puzzle{
-            values:  [[0; DIM]; DIM],
-            isFixed: [[ false; DIM]; DIM],
-            colHasVal: [[ false; DIM ]; DIM ],
-            rowHasVal: [[ false; DIM]; DIM],
-            subgridHasVal: [[[false; DIM ]; SUBGRID_DIM]; SUBGRID_DIM],
+            values:  [[0; VAL_DIM]; DIM],
+            is_fixed: [[ false; DIM]; DIM],
+            col_has_val: [[ false; VAL_DIM ]; DIM ],
+            row_has_val: [[ false; VAL_DIM]; DIM],
+            subgrid_has_val: [[[false; VAL_DIM ]; SUBGRID_DIM]; SUBGRID_DIM],
         }
     }
 }
 
-fn load_puzzle(file_name: &str) -> Puzzle {
-    let mut newPuzzle = Puzzle::default();
-    let mut real_idx: usize = 0;
-    let mut col: usize = 0;
-    let mut row: usize = 0;
-
-    let mut contents = fs::read_to_string(file_name)
-        .expect("An error occurred while reading the file");
-
-
-    contents = contents.trim().to_string();
-
-    for (i, c) in contents.chars().enumerate() {
-        if !c.is_whitespace(){
-            let val: usize = c.to_digit(10).unwrap() as usize;
-            row = real_idx / DIM;
-            col = real_idx % DIM;
-
-            newPuzzle.values[row][col] = val;
-
-            if val != 0 {
-                newPuzzle.isFixed[row][col] = true;
-                newPuzzle.colHasVal[col][val] = true;
-                newPuzzle.rowHasVal[row ][val ] = true;
-                newPuzzle.subgridHasVal[row  / SUBGRID_DIM][col / SUBGRID_DIM][val] = true;
-            }
-            real_idx += 1;
-        }
-    }
-
-    return newPuzzle;
-}
 impl Puzzle{
+
+
+
     fn is_safe(&mut self, val: usize, row: usize, col: usize) -> bool {
-        return !(self.rowHasVal[row][val])
-                && !(self.colHasVal[col][val])
-                && !(self.subgridHasVal[row / 3][col / 3][val])
-                && !(self.isFixed[row][col]);
+        return !(self.row_has_val[row][val])
+                && !(self.col_has_val[col][val])
+                && !(self.subgrid_has_val[row / 3][col / 3][val])
+                && !(self.is_fixed[row][col]);
     }
 
     fn place_val(&mut self, val: usize, row: usize, col: usize){
         self.values[row][col] = val;
-        self.subgridHasVal[row / SUBGRID_DIM][col / SUBGRID_DIM][val] = true;
-        self.rowHasVal[row][val] = true;
-        self.colHasVal[col][val] = true;
+        self.subgrid_has_val[row / SUBGRID_DIM][col / SUBGRID_DIM][val] = true;
+        self.row_has_val[row][val] = true;
+        self.col_has_val[col][val] = true;
     }
 
     fn remove_val(&mut self, val: usize, row: usize, col: usize){
         self.values[row][col] = 0;
-        self.subgridHasVal[row / SUBGRID_DIM][col / SUBGRID_DIM][val] = false;
-        self.rowHasVal[row][val] = false;
-        self.colHasVal[col][val] = false;
+        self.subgrid_has_val[row / SUBGRID_DIM][col / SUBGRID_DIM][val] = false;
+        self.row_has_val[row][val] = false;
+        self.col_has_val[col][val] = false;
     }
 
     pub fn solve(&mut self, n: usize) -> bool {
         if n == 81 {
             return true;
         }
+
         let row: usize = n / 9;
         let col: usize = n % 9;
 
-        if self.isFixed[row][col] {
+        if self.is_fixed[row][col] {
             if self.solve(n + 1) {
                 return true;
             }
         }
+        // nobody told me this is the same as i = 1; i < 10
+        for val in 1..10 {
 
-        for val in 1..9 {
             if self.is_safe(val, row, col) {
+
                 self.place_val(val, row, col);
 
                 if self.solve(n + 1) {
@@ -129,11 +103,48 @@ impl Puzzle{
     }
 }
 
+fn load_puzzle(file_name: &str) -> Puzzle {
+    let mut new_puzzle = Puzzle::default();
+    let mut real_idx: usize = 0;
+    let mut col: usize;
+    let mut row: usize;
 
+    let mut contents = fs::read_to_string(file_name)
+        .expect("An error occurred while reading the file");
+
+
+    contents = contents.trim().to_string();
+
+    for c in contents.chars() {
+        if !c.is_whitespace(){
+            let val: usize = c.to_digit(10).unwrap() as usize;
+            row = real_idx / DIM;
+            col = real_idx % DIM;
+
+            new_puzzle.values[row][col] = val;
+
+            if val != 0 {
+                new_puzzle.is_fixed[row][col] = true;
+                new_puzzle.col_has_val[col][val] = true;
+                new_puzzle.row_has_val[row ][val ] = true;
+                new_puzzle.subgrid_has_val[row  / SUBGRID_DIM][col / SUBGRID_DIM][val] = true;
+            }
+            real_idx += 1;
+        }
+    }
+
+    return new_puzzle;
+}
 
 fn main(){
-    let mut puzzle = load_puzzle("puzzle_test.txt");
-    puzzle.solve(0);
+
+    let args: Vec<String> = env::args().collect();
+    let mut puzzle = load_puzzle(&args[1]);
+    println!("Original Puzzle: ");
     puzzle.print_puzzle();
 
+    puzzle.solve(0);
+
+    println!("\nSolution:");
+    puzzle.print_puzzle();
 }
